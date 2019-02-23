6476bbc102d0f3819315f5feead424bafbb90bdc
praveen s <pravens97@gmail.com>
2019-02-23 14:37:00 +0530

    message

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    return "hello....."
+
+@app.route("/home")
+def home():
+    return "my home page"
+
+@app.route("/contact")
+def contact():
+    return "my contact"
+
+if(__name__=="__main__"):
+    app.run()
